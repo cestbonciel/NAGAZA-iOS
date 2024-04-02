eac86a5c20670bcc246586872dd6fb61d6cde5c6
LeeSeungmin <dltmdals0608@naver.com>
2024-04-02 18:59:14 +0900

    [#31]Refactor: apple map -> naver map으로 마이그레이션
    - 필요없는 코드 삭제
    - searchItem 바인딩 변수 추가
    - place에 대한 지역명과 색상 변경 함수 추가

--- Nagaza/Sources/Presentation/Feature/TabBar/Map/View/MapViewController.swift
+++ Nagaza/Sources/Presentation/Feature/TabBar/Map/View/MapViewController.swift
@@ -6,22 +6,23 @@
 //
 
 import UIKit
-import MapKit
 
 import RxSwift
 import RxCocoa
 import RxGesture
+import NMapsMap
+
 
 final class MapViewController: NagazaBaseViewController {
     var viewModel: MapViewModel!
     
-    private lazy var mapView: MKMapView = {
-        let mapView = MKMapView()
-        mapView.showsUserLocation = true
+    private let mapView: NMFMapView = {
+        let mapView = NMFMapView()
+        mapView.positionMode = .direction
         return mapView
     }()
     
-    private lazy var mapSearchView = MapSearchView()
+    private let mapSearchView = MapSearchView()
     
     static func create(with viewModel: MapViewModel) -> MapViewController {
         let vc = MapViewController()
@@ -69,34 +70,24 @@ final class MapViewController: NagazaBaseViewController {
             .disposed(by: disposeBag)
         
         output.searchItem
-            .drive(with: self) { owner, item in
-                guard let item = item,
-                      let latitude = Double(item.y),
-                      let longitude = Double(item.x) else { return }
-                owner.updateLocation(
-                    with: CLLocationCoordinate2D(
-                        latitude: latitude,
-                        longitude: longitude
-                    )
-                )
-            }
+            .drive(self.rx.searchItem)
             .disposed(by: disposeBag)
     }
     
-    private func updateLocation(with coordinates: CLLocationCoordinate2D?) {
-        guard let coordinates = coordinates else { return }
-        let region = MKCoordinateRegion(
-            center: coordinates,
-            latitudinalMeters: 500,
-            longitudinalMeters: 500
-        )
-        mapView.region = region
+    func updateTextLabel(text: String) {
+        self.mapSearchView.textLabel.text = text
+        self.mapSearchView.textLabel.textColor = NagazaAsset.Colors.black.color
+    }
+    
+    func updateLocation(with coordinates: CLLocationCoordinate2D) {
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: coordinates.latitude, lng: coordinates.longitude))
+        self.mapView.moveCamera(cameraUpdate)
     }
 }
 
 extension MapViewController {
     private func setupDelegate() {
-        mapView.delegate = self
+        //        mapView.delegate = self
     }
     
     private func locationManagerDidChangeAuthorization() {
@@ -125,21 +116,19 @@ extension MapViewController {
     }
 }
 
-extension MapViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        updateLocation(with: userLocation.coordinate)
+extension Reactive where Base: MapViewController {
+    var searchItem: Binder<Place?> {
+        return Binder(self.base) { base, item in
+            guard let item = item,
+                  let latitude = Double(item.y),
+                  let longitude = Double(item.x) else { return }
+            base.updateTextLabel(text: item.placeName)
+            base.updateLocation(
+                with: CLLocationCoordinate2D(
+                    latitude: latitude,
+                    longitude: longitude
+                )
+            )
+        }
     }
 }
-
-//#if DEBUG
-//import SwiftUI
-//
-//struct MapViewControllerPreview: PreviewProvider {
-//    static var previews: some View {
-//        let actions = MapViewModelActions()
-//        let viewModel = MapViewModel(actions: actions)
-//        let viewController = MapViewController.create(with: viewModel)
-//        return viewController.toPreView()
-//    }
-//}
-//#endif
